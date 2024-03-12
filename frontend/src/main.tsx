7f9c68302a0d51ee88b772ff30a05b769bc38075
Foxster <huythefox@gmail.com>
2024-03-11 23:17:31 -0700

    Create layout Co-authored-by: Ton Huynh <thanhtonhuynh@users.noreply.github.com>

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,53 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import 'bootstrap/dist/css/bootstrap.css'
+import { Outlet, RouterProvider, createBrowserRouter, Navigate } from 'react-router-dom';
+
+// Global styles
+import './index.css';
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.css';
+
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Homepage from './pages/Homepage/Homepage';
+import NotFoundPage from './pages/NotFoungPage/NotFoundPage';
+
+function Layout() {
+  return (
+    <>
+      <Header />
+
+      <main>
+        <Outlet />
+      </main>
+
+      <Footer />
+    </>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Homepage />
+      },
+      {
+        path: 'error-not-found',
+        element: <NotFoundPage />
+      },
+      {
+        path: '*',
+        element: <NotFoundPage />
+      }
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ frontend/src/pages/NotFoungPage/NotFoundPage.tsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import { IoCaretBackOutline } from "react-icons/io5";
+
+import "./NotFoundPage.css";
+
+const NotFoundPage = () => {
+  return (
+    <section className="d-flex flex-column not-found">
+      <h1>404 Not found!</h1>
+      <p>It's okay that you got lost!</p>
+      <p>The page that you're looking for doesn't exist at the moment.</p>
+      <Link to="/" className="link">
+        <IoCaretBackOutline />
+        Back to Home
+      </Link>
+    </section>
+  );
+};
+
+export default NotFoundPage;
