98c60d6bb7c5a1a094b6dc9d93f2841beca9b535
Foxster <huythefox@gmail.com>
2024-03-13 22:44:16 -0700

    Add sign up route

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,19 @@
-require('dotenv').config();
+// Node modules
 const https = require('https');
 const fs = require('fs');
+const path = require('path');
+
+// Third-party modules
+require('dotenv').config();
 const express = require('express');
+const validator = require('validator');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+
+// Local modules
 const { Message, User, Chat, Comment, Project, List, Task, File } = require('./db');
 
+// Server
 const port = process.env.PORT || 8080;
 const options = {
     key: fs.readFileSync(process.env.SSL_KEY_PATH),
@@ -11,20 +21,65 @@ const options = {
 };
 const app = express();
 const server = https.createServer(options, app);
+const saltRounds = 10; // For bcrypt
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// Static files
+app.use("/test", express.static(path.join(__dirname, './test.local')),); // http://localhost:8080/test/
+// Session
+app.use(session({
+    name: 'cmpt372project.sid',
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+}));
 
+// Routes
 app.get('/', (req, res) => {
-    res.send('<h1>Hello, World!</h1>');
-    // res.send('Hello, World!');
+    if (req.session.user) {
+        res.send(`<h1>Hello, ${req.session.user.name}!</h1>`);
+    } else {
+        res.send('<h1>Hello, World!</h1>');
+    }
 });
 
 // Get all users to check if database is working
 app.get('/users', async (req, res) => {
     const users = await User.findAll();
     res.json(users);
+});
+
+app.post('/signup', async (req, res) => {
+    const { name, email, password, passwordConfirmation } = req.body;
+
+    // Validate email
+    if (!validator.isEmail(email)) {
+        return res.status(400).json({ message: 'Invalid email' });
+    }
+
+    // Check if email is already used
+    const existingUser = await User.findOne({ where: { email } });
+    if (existingUser) {
+        return res.status(400).json({ message: 'Email already used' });
+    }
+    
+    // Validate password
+    if (password !== passwordConfirmation) {
+        return res.status(400).json({ message: 'Passwords do not match' });
+    }
+
+    // Hash password
+    bcrypt.hash(password, saltRounds, async (err, hash) => {
+        if (err) {
+            return res.status(500).json({ message: 'Internal server error' });
+        }
+        // Create user
+        const user = await User.create({ name, email, password: hash });
+        req.session.user = user;
+        res.json(user);
+    });
 });
 
 // app.listen(port, () => console.log(`Server is running on port ${port}`));
