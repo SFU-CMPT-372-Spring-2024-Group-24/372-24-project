80dd55411e854d2463c37ed07e8e5727be1c315d
Ton Huynh <kintonhtt@gmail.com>
2024-03-14 21:17:40 -0700

    Add signup logic and make login and signup into 2 components

--- frontend/src/pages/SignupLoginpage/LoginSignup.tsx
+++ frontend/src/pages/SignupLoginpage/LoginSignup.tsx
@@ -1,68 +1,18 @@
-import { useContext, useState } from "react";
-import { FaUser } from "react-icons/fa";
-import { MdEmail } from "react-icons/md";
-import { RiLockPasswordFill } from "react-icons/ri";
+import { useState } from "react";
 import { FcGoogle } from "react-icons/fc";
 import { FaHandshakeSimple } from "react-icons/fa6";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+import LoginForm from "../../components/Forms/LoginForm";
+import SignupForm from "../../components/Forms/SignupForm";
 
 // Styles
 import "./LoginSignup.scss";
 
-// Contexts
-import UserContext from "../../contexts/UserContext";
-
 const LoginSignup = () => {
-  const userContext = useContext(UserContext);
-  const navigate = useNavigate();
-
   const [action, setAction] = useState("Sign Up");
-  const [userIdentifier, setUserIdentifier] = useState<string>("");
-  const [userPass, setUserPass] = useState<string>("");
-  const [userName, setUserName] = useState<string>("");
 
-  const handleActionChange = (newAction: string) => {
-    setAction(newAction);
-  };
-
-  const handleCreateAccount = () => {
-    // Create account logic
-  };
-
-  const handleSignIn = async () => {
-    // Debug
-    console.log("Signing in...");
-    console.log("Email: ", userIdentifier);
-    console.log("Password: ", userPass);
-    
-    const response = await fetch("/api/users/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        identifer: userIdentifier,
-        password: userPass,
-      }),
-    });
-    
-    if (response.ok) {
-      const user = await response.json();
-      
-      // Debug
-      console.log("Logged in:");
-      console.log(user); 
-
-      if (userContext) {
-        userContext.setUser(user);
-      }
-
-      navigate("/");
-    }
-    else {
-      console.error("Login failed: ", response.status, response.statusText);
-    }
-  };
+  const handleActionChange = (newAction: string) => setAction(newAction);
 
   const handleGoogleSignIn = () => {
     // Google Sign in logic
@@ -85,53 +35,7 @@ const LoginSignup = () => {
           <div className="underline"></div>
         </div>
 
-        <div className="inputs">
-          {action === "Sign Up" && (
-            <div className="input">
-              <FaUser size={18} />
-              <input
-                id="user_name"
-                name="user_name"
-                type="text"
-                value={userName}
-                onChange={(e) => setUserName(e.target.value)}
-                placeholder="Your display name"
-              />
-            </div>
-          )}
-          <div className="input">
-            <MdEmail size={20} />
-            <input
-              id="user_identifier"
-              name="user_identifier"
-              type="text"
-              value={userIdentifier}
-              onChange={(e) => setUserIdentifier(e.target.value)}
-              placeholder="Email"
-            />
-          </div>
-          <div className="input">
-            <RiLockPasswordFill size={20} />
-            <input
-              id="user_pass"
-              name="user_pass"
-              type="password"
-              value={userPass}
-              onChange={(e) => setUserPass(e.target.value)}
-              placeholder="Password"
-            />
-          </div>
-        </div>
-
-        {action === "Sign Up" ? (
-          <button className="action-button" onClick={handleCreateAccount}>
-            Create Account
-          </button>
-        ) : (
-          <button className="action-button" onClick={handleSignIn}>
-            Log In
-          </button>
-        )}
+        {action === "Sign Up" ? <SignupForm /> : <LoginForm />}
 
         <div className="or">— Or —</div>
 
