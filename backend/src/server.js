359687ed88f9128df11b83c071919f8ec7af0e78
Foxster <huythefox@gmail.com>
2024-03-20 20:03:21 -0700

    Use HTTP server only

--- backend/src/server.js
+++ backend/src/server.js
@@ -55,27 +55,27 @@ app.use("/lists", require("./routes/lists"));
 app.use("/tasks", require("./routes/tasks"));
 
 // HTTPS server (for API)
-const options = {
-  key: fs.readFileSync(process.env.SSL_KEY_PATH),
-  cert: fs.readFileSync(process.env.SSL_CERT_PATH),
-  requestCert: false,
-  rejectUnauthorized: false,
-};
-const httpsPort = process.env.HTTPS_PORT || 8443;
-const httpsServer = https.createServer(options, app);
-httpsServer.listen(httpsPort, () =>
-  console.log(`HTTPS server is running on port ${httpsPort}`)
-);
+// const options = {
+//     key: fs.readFileSync(process.env.SSL_KEY_PATH),
+//     cert: fs.readFileSync(process.env.SSL_CERT_PATH),
+//     requestCert: false,
+//     rejectUnauthorized: false,
+// };
+// const httpsPort = process.env.HTTPS_PORT || 8443;
+// const httpsServer = https.createServer(options, app);
+// httpsServer.listen(httpsPort, () =>
+//     console.log(`HTTPS server is running on port ${httpsPort}`)
+// );
 
 // HTTP server (for chatting only)
 const httpPort = process.env.HTTP_PORT || 8080;
-const httpServer = http.createServer();
+const httpServer = http.createServer(app);
 httpServer.listen(httpPort, () =>
-  console.log(`HTTP server is running on port ${httpPort}`)
+    console.log(`HTTP server is running on port ${httpPort}`)
 );
 
 // Chat server
-const io = new SocketIOServer(httpsServer, {
+const io = new SocketIOServer(httpServer, {
     cors: {
         //which url is making calls to our socket io server
         //where our react application is running
@@ -84,30 +84,30 @@ const io = new SocketIOServer(httpsServer, {
     },
 });
 io.on("connection", (socket) => {
-  console.log(`User Connected: ${socket.id}`);
-  // this would be the chat_id instead of room
-  socket.on("join_room", (data) => {
-    socket.join(data);
-    console.log(`User with ID: ${socket.id} joined room: ${data}`);
-  });
+    console.log(`User Connected: ${socket.id}`);
+    // this would be the chat_id instead of room
+    socket.on("join_room", (data) => {
+        socket.join(data);
+        console.log(`User with ID: ${socket.id} joined room: ${data}`);
+    });
 
-  socket.on("send_message", (data) => {
-    socket.to(data).emit("receive_message");
-  });
-  socket.on("disconnect", () => {
-    console.log("User Disconnected", socket.id);
-  });
+    socket.on("send_message", (data) => {
+        socket.to(data).emit("receive_message");
+    });
+    socket.on("disconnect", () => {
+        console.log("User Disconnected", socket.id);
+    });
 });
 
 // Firebase cloud storage
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 const firebaseApp = initializeApp(firebaseConfig);
 const storage = getStorage(firebaseApp);
